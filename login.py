cd08628a203e39a85e574ea58f34e364e783eb2c
shadeesitoo <yaemiko.nyassu@gmail.com>
2025-09-17 19:52:09 -0400

    commit del login

--- login.py
+++ login.py
@@ -1,1 +1,10 @@
-print ("login")
+print ("login v2"") 
+#git stash es un commit temporal, como para código sin terminar
+#Se genera una dirección o stash igual que con el commit
+#Git stash list es para ver la lista de los stash temporales
+#git stash pop es para recibir todos los cambios temporales que dejamos
+
+#git stash 
+#git stash pop
+#git stash list
+#git stash drop
